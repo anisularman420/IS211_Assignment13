9dcb0a3c794f49fb944c5c7f8497ea681b644acb
anisularman420 <143449791+anisularman420@users.noreply.github.com>
2023-11-29 21:35:45 -0500

    Create recursion.py
    Fibonacci Sequence, Euclid's GCD Alg & string comparison

---
+++ recursion.py
@@ -0,0 +1,9 @@
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input. Please provide a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
